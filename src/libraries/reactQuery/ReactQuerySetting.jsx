00e66927e6ecee695c26eee11ba9c09b9739e396
kimzini <rlawlsdl11111@gmail.com>
2025-01-13 02:33:30 +0900

    chore: TanStack Query 세팅

---
+++ src/libraries/reactQuery/ReactQuerySetting.jsx
@@ -0,0 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const MINUTES = 1000 * 60;
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      retryDelay: 1000,
+      staleTime: 5 * MINUTES,
+    },
+  },
+});
+
+export default function ReactQuerySetting(children) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      {children}
+    </QueryClientProvider>
+  );
+}

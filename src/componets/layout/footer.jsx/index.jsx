4a5a9ac1503888db95ef0788b89f45f0556f8fcc
Seyeonnnn <144412217+Seyeonnnn@users.noreply.github.com>
2025-01-14 21:19:24 +0900

    feat:footer 컴포넌트, iconbutton 생성

--- src/componets/layout/footer.jsx/index.jsx
+++ src/componets/layout/footer.jsx/index.jsx
@@ -1,1 +1,37 @@
-//footer 컴포턴트
+import React from "react";
+import IconButton from "@/componets/common/IconButton";
+import { useNavigate } from "react-router-dom";
+
+const Footer = () => {
+  const navigate = useNavigate();
+  return (
+    <footer className="absolute bottom-0 left-0 w-full h-24 bg-white flex justify-around items-center">
+      <IconButton
+        iconSrc="/icons/homebutton.png"
+        altText="HOME"
+        label="홈"
+        onClick={() => navigate("/")}
+      />
+      <IconButton
+        iconSrc="/icons/framemakebutton.png"
+        altText="프레임 만들기"
+        label="프레임 만들기"
+        onClick={() => navigate("/")}
+      />
+      <IconButton
+        iconSrc="/icons/cabinetbutton.png"
+        altText="보관함"
+        label="보관함"
+        onClick={() => navigate("/")}
+      />
+      <IconButton
+        iconSrc="/icons/mypagebutton.png"
+        altText="마이페이지"
+        label="마이페이지"
+        onClick={() => navigate("/")}
+      />
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Main/page.jsx
+++ src/pages/Main/page.jsx
@@ -1,7 +1,11 @@
+import React from "react";
+import Footer from "@/componets/layout/footer.jsx";
+
 export const MainPage = () => {
   return (
     <div className="flex items-center justify-center">
       <div className="flex text-3xl">INFRAME</div>
+      <Footer />
     </div>
   );
 };

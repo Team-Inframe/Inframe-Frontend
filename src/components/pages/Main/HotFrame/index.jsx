4e8d6bbc08912e3d1ad55ea0ec88cd5656fbfa21
Seyeonnnn <144412217+Seyeonnnn@users.noreply.github.com>
2025-01-16 01:14:38 +0900

    feat: hotframe 컴포넌트 버튼으로 변경

--- src/components/pages/Main/HotFrame/index.jsx
+++ src/components/pages/Main/HotFrame/index.jsx
@@ -1,11 +1,13 @@
-export const HotFrame = ({ label }) => {
+export const HotFrame = ({ onClick, label }) => {
   return (
-    <div className="flex-col">
-      <div className="bg-white w-44 h-48 shadow-lg border-black  ">
-        <span className="text-black text-l flex flex-col justify-start pl-5">
-          {label}
-        </span>
-      </div>
+    <div className="flex-col ">
+      <button
+        className="bg-white w-44 h-48 shadow-lg border-black"
+        onClick={onClick}
+      ></button>
+      <span className="text-black text-l flex flex-col justify-start">
+        {label}
+      </span>
     </div>
   );
 };
--- src/pages/Main/page.jsx
+++ src/pages/Main/page.jsx
@@ -1,8 +1,10 @@
 import Footer from "@/components/layout/Footer/index.jsx";
+import { useNavigate } from "react-router-dom";
 import { RecommendCard } from "@/components/pages/Main/RecommendCard";
 import { HotFrame } from "@/components/pages/Main/HotFrame";
 
 export const MainPage = () => {
+  const navigate = useNavigate();
   return (
     <div className="absolute flex flex-col items-center h-full w-screen max-w-[490px] justify-center scroll-auto">
       <div className="absolute top-12 w-full pl-9 justify-start flex-col text-left">
@@ -16,7 +18,7 @@ export const MainPage = () => {
         <div className="flex text-s font-thin">많은 사람이 이용했어요!</div>
         <div className="text-l font-semibold ">지금 제일 핫한 프레임</div>
         <div className=" grid grid-cols-2 gap-4">
-          <HotFrame label="지브리st 프레임" />
+          <HotFrame label="지브리st 프레임" onClick={() => navigate("/")} />
         </div>
       </div>
       <Footer />

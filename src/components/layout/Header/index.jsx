be7ace96332554cc7261822f48bd629e9daccea7
kimzini <rlawlsdl11111@gmail.com>
2025-01-20 19:24:58 +0900

    feat: 헤더에 완료 버튼 추가

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,14 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import leftarrow from "/src/assets/svgs/LeftArrow.svg";
 
-const Header = ({ title }) => {
+const Header = ({ title, isCompletedPage }) => {
   const navigate = useNavigate();
   return (
-    <header className="">
-      <button className="bg-cyan-200" onClick={() => navigate(-1)}>
-        <img src={leftarrow} alt="뒤로가기" />
+    <header>
+      <button onClick={() => navigate(-1)}>
+        <img src={leftarrow} alt="뒤로가기" className="mb-[8px] px-[10px]" />
       </button>
-      <div className="Headline_B bg-gray-700">{title}</div>
+      <div className="flex justify-between px-[24px]">
+        <span className="Headline_B">{title}</span>
+        {isCompletedPage && (
+          <button className="Label_L text-syscolor-SystemGray">완료</button>
+        )}
+      </div>
     </header>
   );
 };

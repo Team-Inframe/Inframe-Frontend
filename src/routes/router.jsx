2e199dbbe9a717b5d543ae4ddcac7b51ff8b00a4
김민석 <kimminseok@gimminseog-ui-noteubug.local>
2025-01-20 20:27:36 +0900

    feat:로그인 페이지 구현

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,17 @@
 import { MainPage } from "@/pages/Main/page";
+import LoginPage from "@/pages/LoginPage/page";
 import { createBrowserRouter } from "react-router-dom";
+import { Children } from "react";
+import { element } from "prop-types";
 
 const routes = [
   {
     path: "/",
     children: [{ index: true, element: <MainPage /> }],
+  },
+  {
+    path: "/login",
+    children: [{ index: true, element: <LoginPage /> }],
   },
 ];
 

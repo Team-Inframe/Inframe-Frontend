684071c8b98858bc5b6e446a711398014e42fd10
turtlejun05 <psj_0504@naver.com>
2025-01-21 16:11:43 +0900

    feat:env루트 설정

--- src/api/users.js
+++ src/api/users.js
@@ -1,6 +1,7 @@
+// @ts-nocheck
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = `${import.meta.env.VITE_BASE_URL}/api/v1`;
 
 export const login = async (email, password) => {
   try {

8e2edfc1d1d8939bc05ec661cd8ac2dd821c07bb
turtlejun05 <psj_0504@naver.com>
2025-01-21 22:06:47 +0900

    feat: env 루트설정

--- src/api/customframes.js
+++ src/api/customframes.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // @ts-ignore
-const BASE_URL = `${import.meta.env.VITE_BASE_URL}/api/v1`;
+const BASE_URL = `${import.meta.env.VITE_BASE_URL}`;
 
 export const createCustomFrame = async (
   userId,

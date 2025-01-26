e1f3039c4b6c6fcda01524c518baf38d093ba2db
kimzini <rlawlsdl11111@gmail.com>
2025-01-26 15:14:31 +0900

    design: footer 디테일 추가

--- src/components/common/Button/IconButton/index.jsx
+++ src/components/common/Button/IconButton/index.jsx
@@ -1,7 +1,7 @@
-const IconButton = ({ iconSrc, altText, label, onClick }) => {
+const IconButton = ({ iconSrc, altText, label, onClick, className }) => {
   return (
     <button
-      className="flex h-14 w-11 flex-col items-center justify-center bg-white"
+      className={`flex h-14 w-11 flex-col items-center justify-center bg-white ${className}`}
       onClick={onClick}
     >
       <img src={iconSrc} alt={altText} className="h-7 w-7" />
--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -1,31 +1,64 @@
 import IconButton from "@/components/common/Button/IconButton";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import homebutton from "@/assets/svgs/HomeButton.svg";
 import framemakebutton from "/src/assets/svgs/FrameMakeButton.svg";
 import cabinetbutton from "/src/assets/svgs/CabinetButton.svg";
+import ColoredHome from "@/assets/svgs/ColoredHome.svg";
+import ColoredFrame from "@/assets/svgs/ColoredFrame.svg";
+import ColoredStorage from "@/assets/svgs/ColoredStorage.svg";
 import RoutePath from "@/routes/routePath";
 
 const Footer = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const getActiveButton = () => {
+    switch (location.pathname) {
+      case RoutePath.Main:
+        return "home";
+      case RoutePath.Frame:
+        return "frame";
+      case RoutePath.Storage:
+        return "storage";
+      default:
+        return "home";
+    }
+  };
+
+  const activeButton = getActiveButton();
+
+  const handleClick = (route) => {
+    navigate(route);
+  };
+
   return (
     <footer className="fixed bottom-0 flex h-24 w-screen max-w-[450px] items-center justify-around bg-white">
       <IconButton
-        iconSrc={homebutton}
+        iconSrc={activeButton === "home" ? ColoredHome : homebutton}
         altText="HOME"
         label="홈"
-        onClick={() => navigate(RoutePath.Main)}
+        onClick={() => handleClick(RoutePath.Main)}
+        className={activeButton === "home" ? "text-[#9E70D9]" : "text-gray-600"}
       />
+
       <IconButton
-        iconSrc={framemakebutton}
+        iconSrc={activeButton === "frame" ? ColoredFrame : framemakebutton}
         altText="프레임 만들기"
         label="프레임 만들기"
-        onClick={() => navigate(RoutePath.Frame)}
+        onClick={() => handleClick(RoutePath.Frame)}
+        className={
+          activeButton === "frame" ? "text-[#9E70D9]" : "text-gray-600"
+        }
       />
+
       <IconButton
-        iconSrc={cabinetbutton}
+        iconSrc={activeButton === "storage" ? ColoredStorage : cabinetbutton}
         altText="보관함"
         label="보관함"
-        onClick={() => navigate(RoutePath.Storage)}
+        onClick={() => handleClick(RoutePath.Storage)}
+        className={
+          activeButton === "storage" ? "text-[#9E70D9]" : "text-gray-600"
+        }
       />
     </footer>
   );

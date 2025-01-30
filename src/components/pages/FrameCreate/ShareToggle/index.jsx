975739b45d6408e0787f124cad6ad94314c63ba5
kimzini <rlawlsdl11111@gmail.com>
2025-01-31 01:53:41 +0900

    feat: 공유버튼 적용

--- src/components/pages/FrameCreate/ShareToggle/index.jsx
+++ src/components/pages/FrameCreate/ShareToggle/index.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
-const ShareToggle = () => {
-  const [isOn, setisOn] = useState(false);
+const ShareToggle = ({ onChange }) => {
+  const [isOn, setIsOn] = useState(false);
+
+  const handleToggleChange = () => {
+    setIsOn(!isOn);
+    onChange(!isOn);
+  };
 
   return (
     <div className="flex">
@@ -14,9 +19,7 @@ const ShareToggle = () => {
           readOnly
         />
         <div
-          onClick={() => {
-            setisOn(!isOn);
-          }}
+          onClick={handleToggleChange}
           className="peer h-6 w-11 rounded-full bg-gray-200 from-syscolor-SystemPurple2 to-syscolor-SystemPurple1 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-gradient-to-r peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-syscolor-SystemPurple2"
         ></div>
       </label>
